1ac258aadefbe0c4fc30fd4978bf0a8d00947ef8
Paranoj <e.behterev@yandex.ru>
2022-11-13 20:27:50 +0700

    0.1. FileOutputStream. [#252488 #360212]

--- src/main/java/ru/job4j/io/ResultFile.java
+++ src/main/java/ru/job4j/io/ResultFile.java
@@ -5,17 +5,6 @@ import java.io.IOException;
 import java.util.Arrays;
 
 public class ResultFile {
-
-    public static int[][] multiple(int size) {
-        int[][] array = new int[size][size];
-        for (int row = 0; row < size; row++) {
-            for (int cell = 0; cell < size; cell++) {
-                array[row][cell] = (row + 1) * (cell + 1);
-            }
-        }
-        return array;
-    }
-
     public static void main(String[] args) {
         try (FileOutputStream out = new FileOutputStream("result.txt")) {
             int[][] array = new int[5][5];
