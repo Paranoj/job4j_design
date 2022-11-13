eb7b6a317fe1af1c2c51785aad39468599cdef64
Paranoj <e.behterev@yandex.ru>
2022-11-13 20:26:49 +0700

    0.1. FileOutputStream. [#252488 #360212]

--- src/main/java/ru/job4j/io/ResultFile.java
+++ src/main/java/ru/job4j/io/ResultFile.java
@@ -1,6 +1,7 @@
 package ru.job4j.io;
 
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.Arrays;
 
 public class ResultFile {
@@ -17,13 +18,16 @@ public class ResultFile {
 
     public static void main(String[] args) {
         try (FileOutputStream out = new FileOutputStream("result.txt")) {
-            String multiplicationTable = Arrays.deepToString(multiple(5));
-            String[] subStr = multiplicationTable.split("],");
-            for (String s : subStr) {
-                out.write(s.getBytes());
+            int[][] array = new int[5][5];
+            for (int row = 0; row < 5; row++) {
+                for (int cell = 0; cell < 5; cell++) {
+                    var arr = array[row][cell];
+                    arr = (row + 1) * (cell + 1);
+                    out.write((arr + " ").getBytes());
+                }
                 out.write(System.lineSeparator().getBytes());
             }
-        } catch (Exception e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
