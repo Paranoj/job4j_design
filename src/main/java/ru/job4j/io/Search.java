44cd6f3d2899049d03005239230d23a64ad09bc7
Paranoj <e.behterev@yandex.ru>
2022-11-18 12:29:44 +0700

    5. Валидация параметров запуска. [#246865]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -5,17 +5,34 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
+import java.util.Objects;
 import java.util.function.Predicate;
 
 public class Search {
     public static void main(String[] args) throws IOException {
-        Path start = Paths.get(".");
-        search(start, p -> p.toFile().getName().endsWith(".js")).forEach(System.out::println);
+        validate(args);
+        Path start = Paths.get(args[0]);
+        search(start, p -> p.toFile().getName().endsWith(args[1])).forEach(System.out::println);
     }
 
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
         SearchFiles searcher = new SearchFiles(condition);
         Files.walkFileTree(root, searcher);
         return searcher.getPaths();
+    }
+
+    public static void validate(String[] args) {
+        if (args.length == 0) {
+            throw new IllegalArgumentException("Root folder is null. Usage ROOT_FOLDER.");
+        }
+        if (!Objects.equals(args[1], ".js")) {
+            throw new IllegalArgumentException("Wrong format added to args.");
+        }
+        if (!Files.isDirectory(Path.of(args[0]))) {
+            throw new IllegalArgumentException("Directory doesn't exist");
+        }
+        if (!Files.exists(Path.of(args[0]))) {
+            throw new IllegalArgumentException("File doesn't exist in the chosen directory.");
+        }
     }
 }
