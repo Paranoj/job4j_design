72c2f5109a69e69f91adbd54e1150d34238a1267
Paranoj <e.behterev@yandex.ru>
2022-12-01 16:49:33 +0700

    0. Join [#6862 #366357]

--- sql/0. Outer Join/outer_and_cross_join.sql
+++ sql/0. Outer Join/outer_and_cross_join.sql
@@ -14,14 +14,25 @@ insert into departments(name) values ('hr'), ('management'),
 insert into employees(name, department_id) values ('Tom', 3), ('Helena', 1),
 ('Emily', 4);
 
-select * from departments d full join employees e on e.department_id = d.id;
+select * from departments d
+full join employees e
+on e.department_id = d.id;
 
-select * from departments d left join employees e 
-on e.department_id = d.id where e.id is null;
+select * from departments d
+left join employees e 
+on e.department_id = d.id
+where e.id is null;
 
-select e.name, d.name from departments d left join employees e on e.department_id = d.id
+select e.name, d.name
+from departments d
+left join employees e
+on e.department_id = d.id
+
 group by e.name, d.name;
-select e.name, d.name from employees e right join departments d on e.department_id = d.id
+select e.name, d.name
+from employees e
+right join departments d
+on e.department_id = d.id
 group by e.name, d.name;
 
 /* gender = true - man;
@@ -38,5 +49,4 @@ insert into teens(name,  gender) values ('Joseph', true), ('Anna', false),
 
 select g1.name Name1, g2.name Name2, (g1.gender or g2.gender) as "couple"
 from teens g1 cross join teens g2
-where g1.name != g2.name and (g1.gender = false or g2.gender = false)
-and (g1.gender or g2.gender) != false;
+where g1.gender != g2.gender and g1.gender = true;
