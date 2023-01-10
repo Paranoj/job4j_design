70443998b9c17ad4fcb90a420564cf4c1d2dfdc0
Paranoj <e.behterev@yandex.ru>
2023-01-10 15:00:04 +0700

    1. Отчеты. [#850]

---
+++ src/test/java/ru/job4j/ood/srp/report/ReportEngineTest.java
@@ -0,0 +1,99 @@
+package ru.job4j.ood.srp.report;
+
+import org.junit.jupiter.api.Test;
+import ru.job4j.ood.srp.currency.Currency;
+import ru.job4j.ood.srp.currency.InMemoryCurrencyConverter;
+import ru.job4j.ood.srp.formatter.DateTimeParser;
+import ru.job4j.ood.srp.formatter.ReportDateTimeParser;
+import ru.job4j.ood.srp.model.Employee;
+import ru.job4j.ood.srp.store.MemStore;
+
+import java.util.Calendar;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ReportEngineTest {
+
+    @Test
+    public void whenOldGenerated() {
+        MemStore store = new MemStore();
+        Calendar now = Calendar.getInstance();
+        Employee worker = new Employee("Ivan", now, now, 100);
+        DateTimeParser<Calendar> parser = new ReportDateTimeParser();
+        store.add(worker);
+        Report engine = new ReportEngine(store, parser);
+        StringBuilder expect = new StringBuilder()
+                .append("Name; Hired; Fired; Salary;")
+                .append(System.lineSeparator())
+                .append(worker.getName()).append(" ")
+                .append(parser.parse(worker.getHired())).append(" ")
+                .append(parser.parse(worker.getFired())).append(" ")
+                .append(worker.getSalary())
+                .append(System.lineSeparator());
+        assertThat(engine.generate(em -> true)).isEqualTo(expect.toString());
+    }
+
+    @Test
+    public void whenReportForAD() {
+        MemStore store = new MemStore();
+        Calendar now = Calendar.getInstance();
+        InMemoryCurrencyConverter inMemoryCurrencyConverter = new InMemoryCurrencyConverter();
+        Employee worker = new Employee("Ivan", now, now, 100);
+        DateTimeParser<Calendar> parser = new ReportDateTimeParser();
+        store.add(worker);
+        Report engine = new ReportEngineForAD(store, parser, inMemoryCurrencyConverter);
+        StringBuilder expect = new StringBuilder()
+                .append("Name; Hired; Fired; Salary;")
+                .append(System.lineSeparator())
+                .append(worker.getName()).append(" ")
+                .append(parser.parse(worker.getHired())).append(" ")
+                .append(parser.parse(worker.getFired())).append(" ")
+                .append(inMemoryCurrencyConverter.convert(Currency.USD, worker.getSalary(), Currency.RUB))
+                .append(System.lineSeparator());
+        assertThat(engine.generate(em -> true)).isEqualTo(expect.toString());
+    }
+
+    @Test
+    public void whenReportForHR() {
+        MemStore store = new MemStore();
+        Employee worker1 = new Employee("George", 500);
+        Employee worker2 = new Employee("Ivan", 100);
+        Employee worker3 = new Employee("Michael", 300);
+        store.add(worker1);
+        store.add(worker2);
+        store.add(worker3);
+        Report engine = new ReportEngineForHR(store);
+        StringBuilder expect = new StringBuilder()
+                .append("Name; Salary;")
+                .append(System.lineSeparator())
+                .append(worker1.getName()).append(" ")
+                .append(worker1.getSalary())
+                .append(System.lineSeparator())
+                .append(worker3.getName()).append(" ")
+                .append(worker3.getSalary())
+                .append(System.lineSeparator())
+                .append(worker2.getName()).append(" ")
+                .append(worker2.getSalary())
+                .append(System.lineSeparator());
+        assertThat(engine.generate(em -> true)).isEqualTo(expect.toString());
+    }
+
+    @Test
+    public void whenReportForDevs() {
+        MemStore store = new MemStore();
+        Calendar now = Calendar.getInstance();
+        Employee worker = new Employee("Ivan", now, now, 100);
+        DateTimeParser<Calendar> parser = new ReportDateTimeParser();
+        store.add(worker);
+        Report engine = new ReportEngineForDevs(store, parser);
+        StringBuilder expect = new StringBuilder()
+                .append("Name; Hired; Fired; Salary;")
+                .append(System.lineSeparator())
+                .append(worker.getName()).append(";")
+                .append(parser.parse(worker.getHired())).append(";")
+                .append(parser.parse(worker.getFired())).append(";")
+                .append(worker.getSalary())
+                .append(System.lineSeparator());
+        assertThat(engine.generate(em -> true)).isEqualTo(expect.toString());
+    }
+}
