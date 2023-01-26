19efa4cc1e8bdb18696cf05653415520784ed2d1
Paranoj <e.behterev@yandex.ru>
2023-01-26 21:50:47 +0700

    0. Принцип разделения интерфейсов [#4916 #385325]

--- src/main/java/ru/job4j/ood/isp/example/SmartPhone.java
+++ src/main/java/ru/job4j/ood/isp/example/SmartPhone.java
@@ -3,9 +3,9 @@ package ru.job4j.ood.isp.example;
 public interface SmartPhone {
     void initConnection();
 
-    void GPS();
+    void gPS();
 
-    void IrDA();
+    void irDA();
 
     /* Нарушение ISP. В данном контексте ИК-порт является устаревшим стандартом передачи данных,
     * но т.к. он объявлен в интерфейсе, его придется заглушить, хоть он и не используется на современных устройствах.*/
